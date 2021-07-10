3a137c6a00ec581b08c571eb36ccff3c4c58bb91
toinfinityNbeyond <the_1975@naver.com>
2021-07-10 15:38:08 +0900

    0710

---
+++ app/src/main/java/practice/CarTest2.java
@@ -0,0 +1,30 @@
+package practice;
+
+class Car {
+  String color;     
+  String gearType;  
+  int door;        
+
+  Car() {
+    this("white", "auto", 4);
+  }
+
+  Car(String color) {
+    this(color, "auto", 4);
+  }
+
+  Car(String color, String gearType, int door) {
+    this.color = color;
+    this.gearType = gearType;
+    this.door = door;
+  }
+}
+class CarTeset2 {
+  public static void main(String[] args) {
+    Car c1 = new Car();   
+    Car c2 = new Car("blue");
+
+    System.out.println("c1.color=" + c1.color + ", gearType=" + c1.gearType+ ", door="+c1.door);
+    System.out.println("c2.color=" + c2.color + ", gearType=" + c2.gearType+ ", door="+c2.door);
+  }
+}
